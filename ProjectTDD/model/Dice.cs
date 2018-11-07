cc1ed53e188a76b8aa69b00049ac45d0b5536415
Custerx <roger.hurtig@hotmail.com>
2018-11-07 22:01:06 +0100

    RED

---
+++ ProjectTDD/model/Dice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectTDD.model
+{
+    class Dice
+    {
+        public int GetValue()
+        {
+            return 0;
+        }
+    }
+}
---
+++ ProjectTDD/test/DiceTest.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+
+namespace ProjectTDD.test
+{
+    public class DiceTest
+    {
+        private model.Dice m_dice;
+
+        public DiceTest()
+        {
+            m_dice = new model.Dice();
+        }
+
+        [Fact]
+        public void GetValue_RandomValue_InRange1to6()
+        {
+            int actual = m_dice.GetValue();
+            int expectedLow = 1;
+            int expectedHigh = 6;
+
+            Assert.InRange(actual, expectedLow, expectedHigh);
+        }
+    }
+}
