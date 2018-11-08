e9a578b38cbcbe75501fe9c02cd41896529e1f39
Custerx <roger.hurtig@hotmail.com>
2018-11-08 12:11:55 +0100

    GREEN

--- ProjectTDD/model/Dice.cs
+++ ProjectTDD/model/Dice.cs
@@ -7,10 +7,21 @@ namespace ProjectTDD.model
     class Dice
     {
         private static Random m_random = new Random();
+        private int m_value;
 
+        public Dice()
+        {
+            m_value = 0;
+        }
+        /*
         public int GetValue()
         {
             return m_random.Next(1, 7);
+        }*/
+
+        public int GetValue()
+        {
+            return m_value;
         }
     }
 }
