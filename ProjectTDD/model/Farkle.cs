dd9542ad350422a57a769b54d157a86783426f4e
Custerx <roger.hurtig@hotmail.com>
2018-11-08 15:04:28 +0100

    RED

---
+++ ProjectTDD/model/Farkle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectTDD.model
+{
+    class Farkle
+    {
+        private Dice m_dice_1;
+        private Dice m_dice_2;
+        private Dice m_dice_3;
+        private Dice m_dice_4;
+        private Dice m_dice_5;
+        private Dice m_dice_6;
+
+        public enum Dices
+        {
+            Dice_1,
+            Dice_2,
+            Dice_3,
+            Dice_4,
+            Dice_5,
+            Dice_6
+        }
+
+        public Farkle()
+        {
+            m_dice_1 = new Dice();
+            m_dice_2 = new Dice();
+            m_dice_3 = new Dice();
+            m_dice_4 = new Dice();
+            m_dice_5 = new Dice();
+            m_dice_6 = new Dice();
+        }
+
+        public List<model.Dice> Play()
+        {
+            List<model.Dice> diceList = new List<model.Dice>();
+
+            return diceList;
+        }
+    }
+}
---
+++ ProjectTDD/test/FarkleTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ProjectTDD.test
+{
+    public class FarkleTest
+    {
+        private model.Farkle m_farkle;
+
+        public FarkleTest()
+        {
+            m_farkle = new model.Farkle();
+        }
+
+        [Fact]
+        public void Play_ListWith6Dices_return6()
+        {
+            List<model.Dice> diceList = m_farkle.Play();
+            int actual = diceList.Count;
+            int expected = 6;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
