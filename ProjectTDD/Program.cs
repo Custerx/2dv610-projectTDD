480a8e0c8502c34f908be5da3b00a65903d909b4
Custerx <roger.hurtig@hotmail.com>
2018-11-20 21:22:07 +0100

    created Program.cs to be able to run debug and code coverage

---
+++ ProjectTDD/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectTDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            view.FarkleView v = new view.FarkleView();
+            controller.Farkle c = new controller.Farkle(v);
+            c.Start();
+        }
+    }
+}
