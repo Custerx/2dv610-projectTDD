10b75811e6e092e686f6d07bf6c6f3970b0c56fa
Custerx <roger.hurtig@hotmail.com>
2018-11-26 15:47:44 +0100

    REFACTOR

--- ProjectTDD/controller/Farkle.cs
+++ ProjectTDD/controller/Farkle.cs
@@ -23,10 +23,17 @@ namespace ProjectTDD.controller
 
         public void Start(bool a_noTest = true)
         {
-            int players = m_IView.GetAmountOfPlayers();
-            List<IPlayer> playerList = CreatePlayer(players);
-            playerList = AddName(playerList);
-            Play(playerList, a_noTest);
+            try
+            {
+                int players = m_IView.GetAmountOfPlayers();
+                List<IPlayer> playerList = CreatePlayer(players);
+                playerList = AddName(playerList);
+                Play(playerList, a_noTest);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
 
         internal List<IPlayer> CreatePlayer(int a_players)
@@ -64,91 +71,110 @@ namespace ProjectTDD.controller
 
                     if (action == view.FarkleView.Action.NewGame)
                     {
-                        if (a_noTest)
-                        {
-                            Start();
-                        }
-
-                        throw new model.exception.ValidateNewGameException();
+                        NewGame(a_noTest);
                     }
 
                     if (action == view.FarkleView.Action.Roll)
                     {
-                        player.Roll();
-                        player.UpdateTotalScore();
-
-                        m_IView.DisplayRolledDices(player.GetPlayername(), player.GetHand(), player.CalculateScore(), player.GetTotalScore());
-
-                        if (a_noTest) // To avoid 2 sec delay when testing.
-                        {
-                            System.Threading.Thread.Sleep(2000);
-                        }
-
-                        if (player.IsPlayerWinner())
-                        {
-                            m_IView.DisplayWinner(player.GetPlayername(), player.GetTotalScore());
-
-                            if (a_noTest) // To avoid 5 sec delay when testing.
-                            {
-                                System.Threading.Thread.Sleep(5000);
-                                Start();
-                            }
-                        }
-
+                        Roll(player, a_noTest);
                         continue;
                     }
 
                     if (action == view.FarkleView.Action.Save)
                     {
-                        List<model.Dice> diceList = player.GetHand();
-                        List<model.Dice> tempDiceList = new List<model.Dice>();
-                        List<model.Dice> dicesToBeSavedList = new List<model.Dice>();
-
-                        for (int i = 0; i < diceList.Count; i++)
-                        {
-                            m_IView.DisplaySaveKeys();
-                            model.Hand.Dices input = m_IView.GetDiceToSave();
-
-                            if (input == model.Hand.Dices.Done)
-                            {
-                                break;
-                            }
-
-                            int index = diceList.FindIndex(d => d.Dicenumber == input);
-
-                            if (index == -1)
-                            {
-                                throw new model.exception.DiceNotFoundException();
-                            }
-                            else
-                            {
-                                model.Dice diceToSave = diceList[index];
-                                tempDiceList.Add(diceToSave);
-                            }
-                        }
-
-                        foreach (model.Dice d in tempDiceList)
-                        {
-                            dicesToBeSavedList.Add(d);
-                        }
-
-                        for (int i = 0; i < dicesToBeSavedList.Count; i++)
-                        {
-                            player.Save(dicesToBeSavedList[i]);
-                        }
+                        Save(player);
                     }
 
                     if (action == view.FarkleView.Action.Quit)
                     {
-                        if (a_noTest)
-                        {
-                            Environment.Exit(0);
-                        }
-
-                        throw new model.exception.ValidateQuitException();
+                        Quit(a_noTest);
                     }
                 }
             } while (a_noTest);
+        }
+
+        private void NewGame(bool a_noTest)
+        {
+            if (a_noTest)
+            {
+                Start();
+            }
+
+            throw new model.exception.ValidateNewGameException();
+        }
+
+        private void Roll(IPlayer player, bool a_noTest)
+        {
+            player.Roll();
+            player.UpdateTotalScore();
+
+            m_IView.DisplayRolledDices(player.GetPlayername(), player.GetHand(), player.CalculateScore(), player.GetTotalScore());
+
+            if (a_noTest) // To avoid 2 sec delay when testing.
+            {
+                System.Threading.Thread.Sleep(2000);
+            }
+
+            if (player.IsPlayerWinner())
+            {
+                m_IView.DisplayWinner(player.GetPlayername(), player.GetTotalScore());
+
+                if (a_noTest) // To avoid 5 sec delay when testing.
+                {
+                    System.Threading.Thread.Sleep(5000);
+                    Start();
+                }
+            }
+        }
+
+        private void Save(IPlayer player)
+        {
+            List<model.Dice> diceList = player.GetHand();
+            List<model.Dice> tempDiceList = new List<model.Dice>();
+            List<model.Dice> dicesToBeSavedList = new List<model.Dice>();
+
+            for (int i = 0; i < diceList.Count; i++)
+            {
+                m_IView.DisplaySaveKeys();
+                model.Hand.Dices input = m_IView.GetDiceToSave();
+
+                if (input == model.Hand.Dices.Done)
+                {
+                    break;
+                }
+
+                int index = diceList.FindIndex(d => d.Dicenumber == input);
+
+                if (index == -1)
+                {
+                    throw new model.exception.DiceNotFoundException();
+                }
+                else
+                {
+                    model.Dice diceToSave = diceList[index];
+                    tempDiceList.Add(diceToSave);
+                }
+            }
+
+            foreach (model.Dice d in tempDiceList)
+            {
+                dicesToBeSavedList.Add(d);
+            }
+
+            for (int i = 0; i < dicesToBeSavedList.Count; i++)
+            {
+                player.Save(dicesToBeSavedList[i]);
+            }
+        }
+
+        private void Quit(bool a_noTest)
+        {
+            if (a_noTest)
+            {
+                Environment.Exit(0);
+            }
+
+            throw new model.exception.ValidateQuitException();
         }
     }
 }
